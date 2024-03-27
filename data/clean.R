84bda6260fd06473937d6b37513adc9135416257
NGUN MEN TIAL <ngun@ntial.local>
2024-03-27 08:38:34 -0400

    descriptive

--- data/clean.R
+++ data/clean.R
@@ -4,12 +4,21 @@ pacman::p_load(
 here::i_am(
   "covid_sub.csv"
 )
+
+library(dplyr)
 absolute_path<-here::here("covid_sub.csv")
 data<-read.csv(absolute_path,header = TRUE)
-data_clean<-na.omit(data)
-summary(data_clean)
+data_clean<- data %>%
+  mutate(CLASSIFIED =  ifelse(data$CLASIFFICATION_FINAL >= 1 &
+                                data$CLASIFFICATION_FINAL <=3, 
+                              "Positive", "Inconclusive")
+  )
 
 saveRDS(
   data_clean,"data_clean.rds"
 )
+
+# I used the original data because omitting NA(s) remove all the male patients -Ngun
+
+
 
