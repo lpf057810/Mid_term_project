6052713aa97e7bc6d65dad95f9f80259fc6f23dd
NGUN MEN TIAL <ngun@ntial.local>
2024-03-29 14:43:07 -0400

    tables and graph updated

--- data/clean.R
+++ data/clean.R
@@ -8,17 +8,14 @@ here::i_am(
 library(dplyr)
 absolute_path<-here::here("covid_sub.csv")
 data<-read.csv(absolute_path,header = TRUE)
-data_clean<- data %>%
-  mutate(CLASSIFIED =  ifelse(data$CLASIFFICATION_FINAL >= 1 &
-                                data$CLASIFFICATION_FINAL <=3, 
+data_clean<-na.omit(data)
+data_clean<- data_clean %>%
+  mutate(CLASSIFIED =  ifelse(data_clean$CLASIFFICATION_FINAL >= 1 &
+                                data_clean$CLASIFFICATION_FINAL <=3, 
                               "Positive", "Inconclusive")
   )
 
 saveRDS(
   data_clean,"data_clean.rds"
 ) 
-
-# I used the original data because omitting NA(s) remove all the male patients -Ngun
-
-
 
