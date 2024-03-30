eee17e9ae736ed4159d4b6177289e88000a23ca0
Ella Shen <ella.shen@emory.edu>
2024-03-29 23:02:28 -0400

    changes to chisq test

--- subproject1/code/chi-square.R
+++ subproject1/code/chi-square.R
@@ -1,5 +1,5 @@
 pacman::p_load(
-  ggplot2
+  knitr, ggplot2, patchwork
 ) # This code can check whether you have the desired package and load the package after installing.
 
 here::i_am(
@@ -18,30 +18,48 @@ sum_table <- data.frame(matrix(ncol = 4, nrow = 0)) # empty dataframe to store C
 
 for (i in 1:(ncol(data_ctg)-1)) {
   chisq_result <- chisq.test(data_ctg$ICU, data_ctg[[i]])
-  output <- c(names(data_ctg)[i], chisq_result$statistic, chisq_result$parameter, chisq_result$p.value)
+  output <- c(names(data_ctg)[i], round(chisq_result$statistic, 4), chisq_result$parameter, round(chisq_result$p.value, 4))
   sum_table <- rbind(sum_table, output)
 } # for loop that conducts Chi-square tests 
 
 colnames(sum_table) <- c("Variable", "Chi-square", "df", "p-value") # add dataframe column names
 
+chisq_table <- kable(sum_table, caption = "Table. Chi-square Test Results" )
+
 saveRDS(
-  sum_table,
+  chisq_table,
   file = here::here("subproject1/output/chi-square/table.rds")
 ) # save Chi-square test output table to designated location
 
+####################
+
+barcharts <- list()
+
 for (i in 1:(ncol(data_ctg)-1)) {
-  barchart <- ggplot(data_ctg, aes(x = data_ctg[[i]], fill = ICU)) + 
-    geom_bar( position = "dodge") +
-    xlab(names(data_ctg)[i]) +
-    ylab("Count")
-  barchart_name <- paste0("barchart_", names(data_ctg)[i], ".png")
-  ggsave(
-    plot = barchart,
-    device = "png",
-    file.path(here::here("subproject1/output/chi-square",barchart_name)),
-    width=10,
-    height=6
-  )
-} # for loop that creates bar chart for each pair of variables and saves to designated location
+  variable_name <- names(data_ctg)[i]
+  barchart <- ggplot(data_ctg, aes_string(x = variable_name, fill = 'ICU')) + 
+    geom_bar(position = "dodge") +
+    geom_text(stat='count', aes(label=..count..), vjust=-0.5, position=position_dodge(width=0.9)) +
+    xlab(variable_name) +
+    ylab("Count") +
+    ylim(0,4500) +
+    labs(title = paste0("Bar Chart for ", variable_name)) +
+    theme(plot.title = element_text(hjust = 0.5), 
+          plot.margin = margin(6, 6, 6, 6))  
+  
+  
+  barcharts[[i]] <- barchart
+}
 
+design <- "#AABB#
+           CCDDEE"
+layout <- barcharts[[1]] + barcharts[[2]] + barcharts[[3]] + barcharts[[4]] + barcharts[[5]] +
+  plot_layout(design = design)
 
+ggsave(
+  plot = layout,
+  file.path(here::here("subproject1/output/chi-square", "combined_barcharts.png")),
+  width = 20,
+  height = 15,  
+  device = "png"
+)
