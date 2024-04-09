697de4c9f32a0f91faf23b607b7e2522a3c669b3
Pengfei Lou <plou3@emory.edu>
2024-04-09 11:12:23 -0400

    modifications

--- subproject2/code/model_evaluation.R
+++ subproject2/code/model_evaluation.R
@@ -7,42 +7,13 @@ pacman::p_load(
   broom,
   pROC
 )
-
 here::i_am(
-  "data/covid_sub.csv"
+  "subproject2/code/model_evaluation.R"
 )
 
-absolute_path <- here::here("data/covid_sub.csv")
-data<-read.csv(absolute_path,header = TRUE)
-
-data_clean <- data %>% 
-  filter(!is.na(ICU))
-
-data_clean <- data_clean %>%
-  mutate(CLASSIFIED =  ifelse(data_clean$CLASIFFICATION_FINAL >= 1 &
-                                data_clean$CLASIFFICATION_FINAL <=3, 
-                              "Positive", "Inconclusive")
-  )
-
-# encode categorical varibles
-
-columns_to_encode <- names(data_clean)[sapply(data_clean, function(column) {
-  all(c('Yes', 'No') %in% unique(column))
-})]
-
-data_clean <- data_clean %>%
-  mutate(across(all_of(columns_to_encode), ~if_else(.x == 'Yes', 1, 0)))
-
-data_clean <- data_clean %>%
-  mutate(SEX = recode(SEX, 'female' = 1, 'male' = 0))
-
-data_clean <- data_clean %>%
-  mutate(CLASSIFIED = recode(CLASSIFIED, 'Positive' = 1, 'Inconclusive' = 0))
-
-# add a new variable DIED to indicate if the patient died 
-
-data_clean <- data_clean %>%
-  mutate(DIED = if_else(!is.na(DATE_DIED), 1, 0))
+data_clean<-readRDS(
+  here::here("data/data_clean.rds")
+)
 
 # logistic regression
 # PREGNANT PNEUMONIA COPD DIABETES CARDIOVASCULAR RENAL_CHRONIC SEX AGE CLASSIFIED DIED as predictive variables
